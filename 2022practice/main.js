c8543d274e840535f0028e6e6cc6aa3db1e3a406
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-02-11 16:58:55 +0900

    2022/02/11
    JSの記述は正しいのですが、chrome自体がweb audio apiの再生を許可しておらず、アラートが出ました。

--- 2022practice/main.js
+++ 2022practice/main.js
@@ -1,3 +1,34 @@
-var aaa = document.getElementsByClassName('.parent');
+var context;
+window.addEventListener('click', init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    const ctx = new AudioContext();
 
-console.log(aaa);
+
+
+const audioElement = document.querySelector("audio");
+// Web Audio API内で使える形に変換
+const track = ctx.createMediaElementSource(audioElement);
+
+document.querySelector("#play").addEventListener("click", () => {
+    if(ctx.state === "suspended") {
+        ctx.resume();
+    }
+    // 出力につなげる
+    track.connect(ctx.destination);
+    audioElement.play();
+});
+
+// audioElementを一時停止する
+document.querySelector("#pause").addEventListener("click", () => {
+    audioElement.pause();
+});
+
+}
+catch(e) {
+  alert('Web Audio API is not supported in this browser');
+}
+
+}
